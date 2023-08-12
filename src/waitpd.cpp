67280771e6e0b3241985eee7b6b9ee6833aa2840
Ahmed Eldesoky <66114762+ahmed-eldesoky284@users.noreply.github.com>
2023-08-12 21:29:39 +0300

    Update waitpd.cpp
    Signed-off-by: Ahmed Eldesoky <66114762+ahmed-eldesoky284@users.noreply.github.com>

--- src/waitpd.cpp
+++ src/waitpd.cpp
@@ -3,16 +3,16 @@ waitpd::waitpd(byte buttonpin,byte outputpin,unsigned long time){
     inpin=buttonpin;
     outpin=outputpin;
     t=time;}
-void waitpd::waitm(){
-      if(!x){
-        c2=millis();x=1;}
-      if(millis()-c2>t||digitalRead(inpin)){
-        digitalWrite(outpin,LOW); x=0;}
-    }
 void waitpd::waits(byte t){
       if(!x){
         c1=millis();x=1;}
       if(millis()-c1>t*1000||digitalRead(inpin)){
         digitalWrite(outpin,LOW); x=0;}
       }
+void waitpd::waitm(){
+      if(!x){
+        c2=millis();x=1;}
+      if(millis()-c2>t||digitalRead(inpin)){
+        digitalWrite(outpin,LOW); x=0;}
+    }
   
